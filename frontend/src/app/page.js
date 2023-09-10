ac09dd7ac648d616c7fd18aa55fdb89fe9ea16d6
toffery <loltoffery@gmail.com>
2023-09-10 19:57:34 +0300

    fix: moved thicknessStep to props of Table component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -38,22 +38,22 @@ export default function Home() {
   return (
     <div className="flex flex-col p-10 max-w-7xl mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
       <div className="overflow-x-auto flex flex-col p-10 max-w-6xl mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <Table ref={ref}/>
+        <Table ref={ref} thicknessStep={thicknessStep}/>
       </div>
       <br />
       <div>
         <button type="button" onClick={handleButtonClick} className="align-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Конвертировать
         </button>
-
       </div>
     </div>
   )
 }
 
 const Table = forwardRef(function Table(props, ref) {
-  const thicknessStep = ['1', '8', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30', '32', '34',
-    '36', '38', '40', '42', '44', '46', '48', '50', '52', '54', '56', '58', '60', '62', '64'];
+  // const thicknessStep = ['1', '8', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30', '32', '34',
+    // '36', '38', '40', '42', '44', '46', '48', '50', '52', '54', '56', '58', '60', '62', '64'];
+  const thicknessStep = props.thicknessStep;
   return (
     <table id='myTable' ref={ref} className="w-full mx-auto table-auto overflow-scroll border text-xs font-light">
       <thead className='text-xs'>
